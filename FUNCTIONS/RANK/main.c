9e9f1987adecaef6b6d831f0003d3f63e8054cf0
hornyking <b1105106@mail.nuk.edu.tw>
2022-12-31 17:21:14 +0800

    rank

---
+++ FUNCTIONS/RANK/main.c
@@ -0,0 +1,19 @@
+#include "Common.h"
+#include "AL_general.h"
+void Rank(CONFIG *config);
+
+
+int main()
+{
+    int unit=8;
+    CONFIG config;
+
+    al_setup();
+    ALLEGRO_DISPLAY*display=al_create_display(160*unit,90*unit);
+    Rank(&config);
+    al_cleanup();
+    return 0;
+}
+
+
+
